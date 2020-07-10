33b4ca1d4c4ad50a5ee253bbaa4af378b2a7661a
liuyong <2647175185@qq.com>
2020-07-10 22:39:20 +0800

    修改test类

--- first-git-test/src/com/dcits/Test.java
+++ first-git-test/src/com/dcits/Test.java
@@ -8,4 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+    public void update(){
+        System.out.println("修改了...");
+    }
 }
